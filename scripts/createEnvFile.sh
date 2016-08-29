7e1f03476c767004dfb6b9258939a70d6f006d03
Naveed Masaud <naveed.butt@coeus-solutions.de>
2016-08-29 10:52:35 +0500

    Update createEnvFile.sh

--- scripts/createEnvFile.sh
+++ scripts/createEnvFile.sh
@@ -1,3 +1,5 @@
+if [ "$DEPLOYMENT_GROUP_NAME" == "live" ]
+then
 cat > /var/www/html/.env <<- _EOF_
 APP_ENV=production
 APP_DEBUG=false
@@ -8,3 +10,18 @@ DB_USERNAME=admin
 DB_PASSWORD=coeus123!
 
 _EOF_
+
+elif [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]
+then
+cat > /var/www/html/.env <<- _EOF_
+APP_ENV=staging
+APP_DEBUG=true
+
+DB_HOST=staging.dbhost.com
+DB_DATABASE=stage
+DB_USERNAME=admin
+DB_PASSWORD=coeus123!
+
+_EOF_
+
+fi
